eacb2d4ede0e11f665790b49ce60e07d4c73b7df
Tishchenko Maksym <Tishchenko_Maksym@epam.com>
2021-11-03 20:33:00 +0200

    first commit

---
+++ pkg/hasher/hasher.go
@@ -0,0 +1,13 @@
+package hasher
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error)  {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(hash), err
+}
+
+func CheckPasswordHash(password, hash string) bool  {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
---
+++ src/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"hasher/pkg/hasher"
+)
+
+//Package hasher implements 2 functions:
+//HashPassword(password string) (string, error),
+//CheckPasswordHash(password, hash string) bool
+
+func main() {
+
+	hash, _ := hasher.HashPassword("golang")
+	checked := hasher.CheckPasswordHash("golang", hash)
+	if checked {
+		fmt.Println("Password is correct")
+	} else {
+		fmt.Println("Password is incorrect")
+	}
+
+}
